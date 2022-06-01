e4e16d3a59095585b083ee83192b7d775175fb1c
GeekyNaved <45042448+GeekyNaved@users.noreply.github.com>
2022-06-01 12:55:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, {useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './component/HomeScreen.js';
+import LinksScreen from './component/LinksScreen.js';
+import SearchScreen from './component/SearchScreen.js';
+import SplashScreen from 'react-native-splash-screen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="SearchScreen"
+          component={SearchScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="LinksScreen"
+          component={LinksScreen}
+          options={{
+            title: 'Links',
+            headerStyle: {
+              backgroundColor: '#6200ee',
+            },
+            headerTintColor: 'white',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
